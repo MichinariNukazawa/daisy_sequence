c1819f7f1f956cae280b946ff524ebe32e92c5a5
MichinariNukazawa <michinari.nukazawa@gmail.com>
2018-03-16 22:16:58 +0900

    menu>Help>debug(develop)

--- daisy_sequence/js/menu.js
+++ daisy_sequence/js/menu.js
@@ -141,6 +141,49 @@ class DaisyIO{
 	}
 };
 
+const debug_menu = {
+	label: 'debug(develop)',
+	submenu: [
+/*
+	{
+		label: 'Reload',
+		accelerator: 'CmdOrCtrl+R',
+		click: function (item, focusedWindow) {
+			if (focusedWindow) focusedWindow.reload()
+		}
+	},
+	{
+		label: 'Toggle Full Screen',
+		accelerator: (function () {
+			if (process.platform === 'darwin') {
+				return 'Ctrl+Command+F'
+			} else {
+				return 'F11'
+			}
+		})(),
+		click: function (item, focusedWindow) {
+			if (focusedWindow) {
+				focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+			}
+		}
+	},
+*/
+	{
+		label: 'Toggle Developer Tools',
+		accelerator: (function () {
+			if (process.platform === 'darwin') {
+				return 'Alt+Command+I'
+			} else {
+				return 'Ctrl+Shift+I'
+			}
+		})(),
+		click: function (item, focusedWindow) {
+			if (focusedWindow) focusedWindow.toggleDevTools()
+		}
+	}
+	]
+};
+
 var template = [
 {
 	label: '&File',
@@ -574,48 +617,6 @@ var template = [
 	},
 	]
 },
-/*
-{
-	label: 'View(debug)',
-	submenu: [
-	{
-		label: 'Reload',
-		accelerator: 'CmdOrCtrl+R',
-		click: function (item, focusedWindow) {
-			if (focusedWindow) focusedWindow.reload()
-		}
-	},
-	{
-		label: 'Toggle Full Screen',
-		accelerator: (function () {
-			if (process.platform === 'darwin') {
-				return 'Ctrl+Command+F'
-			} else {
-				return 'F11'
-			}
-		})(),
-		click: function (item, focusedWindow) {
-			if (focusedWindow) {
-				focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
-			}
-		}
-	},
-	{
-		label: 'Toggle Developer Tools',
-		accelerator: (function () {
-			if (process.platform === 'darwin') {
-				return 'Alt+Command+I'
-			} else {
-				return 'Ctrl+Shift+I'
-			}
-		})(),
-		click: function (item, focusedWindow) {
-			if (focusedWindow) focusedWindow.toggleDevTools()
-		}
-	}
-	]
-},
-*/
 {
 	label: '&Help',
 	role: 'help',
@@ -650,6 +651,9 @@ var template = [
 		label: 'GitHub',
 		click: function () { require('electron').shell.openExternal('https://github.com/MichinariNukazawa/daisy_sequence') }
 	},
+	{type: 'separator'},
+	debug_menu,
+	{type: 'separator'},
 	{
 		label: '&Help',
 		click: function () {
