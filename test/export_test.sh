30163527d0612f12f5c666a79a00397a4af2b29d
MichinariNukazawa <michinari.nukazawa@gmail.com>
2018-11-23 23:25:34 +0900

    move file test/data/ -> fileformat-example/

--- test/export_test.sh
+++ test/export_test.sh
@@ -15,7 +15,7 @@ rm -rf ${OBJECT_DIR}/*
 mkdir -p ${OBJECT_DIR}
 
 # svg
-SOURCE_PATH=./data/test01.daisysequence
+SOURCE_PATH=../fileformat-example/test01.daisysequence
 DST_PATH=${OBJECT_DIR}/sample.svg
 ${BIN} ${SOURCE_PATH} -o ${DST_PATH}
 [ -s ${DST_PATH} ] # file is not zero size
@@ -25,24 +25,24 @@ grep 'width="500"' ${DST_PATH} > /dev/null
 grep 'height="650"' ${DST_PATH} > /dev/null
 
 # png
-#SOURCE_PATH=./data/test01.daisysequence
+#SOURCE_PATH=../fileformat-example/test01.daisysequence
 #DST_PATH=${OBJECT_DIR}/test01.png
 #${BIN} ${SOURCE_PATH} -o ${DST_PATH}
 #[ -s ${DST_PATH} ] # file is not zero size
 #STR=`file ${DST_PATH}` ; [[ "${STR}" =~ "PNG" ]] # file type
 
 # PlantUML(.puml)
-SOURCE_PATH=./data/test01.daisysequence
+SOURCE_PATH=../fileformat-example/test01.daisysequence
 DST_PATH=${OBJECT_DIR}/test01.puml
 ${BIN} ${SOURCE_PATH} -o ${DST_PATH}
 [ -s ${DST_PATH} ] # file is not zero size
 
-SOURCE_PATH=./data/test02.daisysequence
+SOURCE_PATH=../fileformat-example/test02.daisysequence
 DST_PATH=${OBJECT_DIR}/test02.puml
 ${BIN} ${SOURCE_PATH} -o ${DST_PATH}
 [ -s ${DST_PATH} ] # file is not zero size
 
-SOURCE_PATH=./data/test_all_fragments.daisysequence
+SOURCE_PATH=../fileformat-example/test_all_fragments.daisysequence
 DST_PATH=${OBJECT_DIR}/test_fragments.puml
 ${BIN} ${SOURCE_PATH} -o ${DST_PATH}
 [ -s ${DST_PATH} ] # file is not zero size
--- test/export_test_with_plantuml.sh
+++ test/export_test_with_plantuml.sh
@@ -9,30 +9,20 @@ trap 'echo "$0(${LINENO}) ${BASH_COMMAND}"' ERR
 [ 1 -eq $# ]
 BIN=$1
 
-OBJECT_DIR=work/plantuml
+OBJECT_DIR=obj/plantuml
 
 rm -rf ${OBJECT_DIR}/*
 mkdir -p ${OBJECT_DIR}
 
-# PlantUML(.puml)
-SOURCE_PATH=./data/test01.daisysequence
-DST_PATH=${OBJECT_DIR}/test01.puml
-${BIN} ${SOURCE_PATH} -o ${DST_PATH}
-[ -s ${DST_PATH} ] # file is not zero size
-plantuml ${DST_PATH}
-[ -s "${DST_PATH%.puml}.png" ]
+FILES=`find ../fileformat-example -name "*.daisysequence" -type f`
+for F in $FILES; do
+	# PlantUML(.puml)
+	SOURCE_FILEPATH=$F
+	DST_FILEPATH=${OBJECT_DIR}/$(basename ${SOURCE_FILEPATH}).puml
 
-SOURCE_PATH=./data/test02.daisysequence
-DST_PATH=${OBJECT_DIR}/test02.puml
-${BIN} ${SOURCE_PATH} -o ${DST_PATH}
-[ -s ${DST_PATH} ] # file is not zero size
-plantuml ${DST_PATH}
-[ -s "${DST_PATH%.puml}.png" ]
-
-SOURCE_PATH=./data/test_all_fragments.daisysequence
-DST_PATH=${OBJECT_DIR}/test_fragments.puml
-${BIN} ${SOURCE_PATH} -o ${DST_PATH}
-[ -s ${DST_PATH} ] # file is not zero size
-plantuml ${DST_PATH}
-[ -s "${DST_PATH%.puml}.png" ]
+	${BIN} ${SOURCE_FILEPATH} -o ${DST_FILEPATH}
+	[ -s ${DST_FILEPATH} ] # file is not zero size
+	plantuml ${DST_FILEPATH}
+	[ -s "${DST_FILEPATH%.puml}.png" ]
+done
 
