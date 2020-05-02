87cd863941335052976df7554480dc93a0c33c1c
MichinariNukazawa <michinari.nukazawa@gmail.com>
2020-05-02 10:14:00 +0900

    chmod and add depend packages, setup/library_for_ubuntu.sh

--- setup/library_for_ubuntu.sh
+++ setup/library_for_ubuntu.sh
@@ -8,5 +8,16 @@ trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 
 
 # install depend package
-sudo apt install npm node librsvg2-bin -y
+# https://github.com/nodesource/distributions
+if type npm 2>/dev/null 1>/dev/null ; then
+	echo "npm already exist."
+else
+	# sudo apt install npm -y
+	sudo apt install curl -y
+	curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+	sudo apt install -y nodejs
+fi
+
+# install depend package
+sudo apt install librsvg2-bin plantuml -y
 
